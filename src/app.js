e5b0fa17746e599bfb6e9640265643a6005f75dc
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-09-09 17:03:32 -0400

    organized app.js code, added sinon to setup.js file, added winston as the logging library, and created a service layer for logging

--- src/app.js
+++ src/app.js
@@ -5,21 +5,31 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 
+// Create Express application
 const app = express();
 
+// Create middleware logic
+
+// Define morgan options
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption));
-app.use(helmet());
-app.use(cors());
+// Define validation function
+function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!');
-});
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  // move to the next middleware
+  next();
+}
 
-app.use(function errorHandler(error, req, res, next) {
+// Define error handler
+function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === 'production') {
     response = { error: { message: 'server error' } }
@@ -28,6 +38,18 @@ app.use(function errorHandler(error, req, res, next) {
     response = { message: error.message, error }
   }
   res.status(500).json(response);
+}
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+// app.use(express.json()); // Enable if using non-GET endpoints
+// app.use(validateBearerToken); // Enable after adding validation
+// Routers can go here
+app.use(errorHandler);
+
+app.get('/', (req, res) => {
+  res.send('Hello, boilerplate!');
 });
 
 module.exports = app;
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
 const supertest = require('supertest');
+const sinon = require('sinon');
 
 global.expect = expect;
 global.supertest = supertest;
+global.sinon = sinon;
